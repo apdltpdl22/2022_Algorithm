a5edee058ce4a79c3ccd99a9327eb633ddad296b
apdltpdl22 <apdltpdl22@naver.com>
2022-07-10 22:32:02 +0900

    0710

---
+++ 7월/0710.py
@@ -0,0 +1,15 @@
+#10172 개
+
+# print('''|\\_/|
+# |q p|   /}
+# ( 0 )\"\"\"\\
+# |\"^\"`    |
+# ||_/=\\\\__|''')
+
+# 1000 A+B
+# n, m = map(int, input().split())
+# print(n + m)
+
+# 1008 A/B
+n, m = map(int, input().split())
+print(n / m)
