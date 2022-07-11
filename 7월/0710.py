d96e5506402ddd864ae9ef3ade9a10415458e6b4
apdltpdl22 <apdltpdl22@naver.com>
2022-07-11 22:23:06 +0900

    0711

--- 7월/0710.py
+++ 7월/0710.py
@@ -11,5 +11,37 @@
 # print(n + m)
 
 # 1008 A/B
-n, m = map(int, input().split())
-print(n / m)
+# n, m = map(int, input().split())
+# print(n / m)
+
+# 10926
+# case = input()
+# print("{}??!".format(case))
+
+# 18108
+# y = int(input())
+#
+# if y >= 1000 and y <= 3000:
+#     print(y - 543)
+# else:
+#     print('1000과 3000 사이의 수를 입력해주세요.')
+
+
+# 10430
+# A, B, C = map(int, input().split())
+# print((A+B)%C)
+# print(((A%C) + (B%C))%C)
+# print((A*B)%C)
+# print( ((A%C) * (B%C))%C)
+
+# 2588 곱셈
+n = int(input())
+m = input()
+sum = 0
+
+for i in range(2, -1, -1):
+    num = int(m[i]) * n
+    print(num)
+    sum += num * (10 ** (2-i))
+
+print(sum)
