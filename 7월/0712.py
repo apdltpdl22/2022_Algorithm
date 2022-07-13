dd9dc8a624fe1ffb3b1cf1fd645da8210ede1742
apdltpdl22 <apdltpdl22@naver.com>
2022-07-13 22:03:32 +0900

    0713

--- 7월/0712.py
+++ 7월/0712.py
@@ -1,4 +1,4 @@
-# # 1330
+# # 1330 두 수 비교하기
 # A, B = map(int, input().split())
 #
 # if A > B:
@@ -7,8 +7,8 @@
 #     print('==')
 # else:
 #     print('<')
-
-# 9498
+#
+# # 9498 시험 성적
 # n = int(input())
 #
 # if n >= 90 and n <= 100 :
@@ -21,17 +21,17 @@
 #     print('D')
 # else:
 #     print('F')
-
-# # 2753
+#
+# # 2753 윤년
 # year = int(input())
 #
 # if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
 #     print(1)
 # else:
 #     print(0)
-
-# 14681 사분면 고르기
-
+#
+# # 14681 사분면 고르기
+#
 # x = int(input())
 # y = int(input())
 #
@@ -43,18 +43,55 @@
 #     print(3)
 # else:
 #     print(4)
+#
+# # 2884 알람 시계
+#
+# H, M = map(int, input().split())
+#
+# minute = M - 45
+# hour = H
+#
+# if minute < 0:
+#     minute = 60 + minute
+#     hour = H - 1
+#     if hour < 0:
+#         hour = 24 + hour
+#
+# print('{} {}'.format(hour, minute))
 
-# 2884 알람 시계
+# # 2525 오븐 시계
+#
+# H, M = map(int, input().split())
+# add_M = int(input())
+#
+# M += add_M
+#
+# while M >= 60:
+#     M = M - 60
+#     H += 1
+#     if H > 23:
+#         H = 24 - H
+#
+# print('{} {}'.format(H, M))
 
-H, M = map(int, input().split())
+# 2480 주사위 세개
 
-minute = M - 45
-hour = H
+a, b, c = map(int, input().split())
+max_num = 0
 
-if minute < 0:
-    minute = 60 + minute
-    hour = H - 1
-    if hour < 0:
-        hour = 24 + hour
-
-print('{} {}'.format(hour, minute))
+# case 1 세개 같은 것 나오면 10000+(같은눈)*1000원
+if a == b and b == c:
+    print(10000 + a*1000 )
+# case 2 같은 눈 2개만 나오면 1000 + 같은 눈 * 100
+elif a == b or a == c:
+    print(1000 + a*100)
+elif b == c:
+    print(1000 + b*100)
+# case 3 다 다르다면 그 중 가장 큰 눈 * 100
+else:
+    max_num = a
+    if max_num < b:
+        max_num = b
+    if max_num < c:
+        max_num = c
+    print(max_num * 100)
---
+++ 7월/0713.py
@@ -0,0 +1,36 @@
+# # 2739 구구단
+#
+# N = int(input())
+#
+# for i in range(1, 10):
+#     if i == 9:
+#         print('{} * {} = {}'.format(N, i, N * i), end='')
+#     else:
+#         print('{} * {} = {}'.format(N, i, N*i))
+
+
+# # 10950 A+B-3
+# T = int(input())
+# arr = []
+#
+# for test in range(1, T+1):
+#     n, m = map(int, input().split())
+#     arr.append(n+m)
+#
+# for i in range(len(arr)):
+#     if i == len(arr) - 1:
+#         print(arr[i], end='')
+#     else:
+#         print(arr[i])
+
+# 8393 합
+n = int(input())
+sum = 0
+# 1. 그냥 반복문으로 다 더한다.
+for i in range(1, n+1):
+    sum += i
+print(sum)
+
+# 2. 수학 공식을 이용한다.
+sum = n*(n+1) // 2
+print(sum)
