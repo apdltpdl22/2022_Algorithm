fa7a365b194db656f699f09d69c2707062b86964
apdltpdl22 <apdltpdl22@naver.com>
2022-08-02 17:07:32 +0900

    0802

--- 8월/09_재귀.py
+++ 8월/09_재귀.py
@@ -39,9 +39,58 @@
 # ret = Fibo(N)
 # print(ret)
 
-# def recursion(N):
-#     if N == 0:
-#         pass
+# #17478 재귀함수가 뭔가요?
+# def recursion(N, lines):
+#     print(lines + '\"재귀함수가 뭔가요?\"')
+#     if N > 0:
+#         print(lines + '\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.')
+#         print(lines + '마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.')
+#         print(lines + '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"')
+#         recursion(N-1, lines+'____')
+#     else:
+#         print(lines+'\"재귀함수는 자기 자신을 호출하는 함수라네\"')
+#
+#     print(lines+'라고 답변하였지.')
+#     return
 #
 # N = int(input())
 # print('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.')
+# lines = ''
+# recursion(N, lines)
+
+
+# # 2447. 별 찍기 - 10 2792ms https://mgyo.tistory.com/183 참고
+# def recursion(N):
+#     global map
+#     if N == 3:
+#         map[0][:3] = map[2][:3] = [1, 1, 1]
+#         map[1][:3] = [1, 0, 1]
+#         return
+#     else:
+#         a = N // 3
+#         recursion(N // 3)
+#         for i in range(3):
+#             for j in range(3):
+#                 if i == 1 and j == 1:
+#                     continue
+#                 for k in range(a):
+#                     map[a * i + k][a * j: a * (j + 1)] = map[k][:a]
+#
+#         return
+#
+# N = int(input())
+# map = [[0 for _ in range(N)] for z in range(N)]
+# recursion(N)
+# for y in range(N):
+#     for x in range(N):
+#         if map[y][x] == 1:
+#             print('*', end='')
+#         else:
+#             print(' ', end='')
+#     print()
+
+# 11729 하노이탑
+# Hanoi(n)은 H(n-1) * 2 (마지막 원반 옮기기 전, 후) + 1(마지막 원반 옮기기) 인 재귀식이다!
+
+def Hanoi(N, start, end, sub):
+    pass
